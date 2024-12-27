8333de5a08c8dd2df613f02b4573d22a55bf6fe1
wewantthefunk <wewantthefunk@yahoo.com>
2024-12-27 16:12:54 -0500

    installation

---
+++ wsgi.py
@@ -0,0 +1,26 @@
+from hydra import app
+import constants, utilities
+from flask_mail import Mail
+
+if __name__ == '__main__':
+
+    if len(sys.argv) < 2:
+        print("Path to SQLite database file required!")
+        exit(1000)
+
+    constants.DB_LOCATION = sys.argv[1]
+    
+    utilities.load_private_key()
+    mailInfo = utilities.load_mail_server_info()
+
+    app.config['MAIL_SERVER']=mailInfo['server']
+    app.config['MAIL_PORT'] = mailInfo['port']
+    app.config['MAIL_USERNAME'] = mailInfo['uname']
+    app.config['MAIL_PASSWORD'] = mailInfo['password']
+    app.config['MAIL_USE_TLS'] = True
+    constants.MAIL = Mail(app)
+
+    portJson = utilities.load_json_file('private/port.json')
+    hostJson = utilities.load_json_file('private/url.json')
+
+    app.run(debug=True, port=portJson['port'], host=hostJson['url'])
