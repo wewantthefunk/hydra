ea0ccfff19209e4b1f862b6a877c3b7ecc56c418
wewantthefunk <wewantthefunk@yahoo.com>
2024-12-27 17:31:02 -0500

    installation

--- install-gunicorn.sh
+++ install-gunicorn.sh
@@ -49,15 +49,15 @@ sudo systemctl enable ghydra
 
 #configure nginx
 
-sudo echo "server {" > /etc/nginx/sites/available/ghydra
-sudo echo "  listen 80;" >> /etc/nginx/sites/available/ghydra
-sudo echo "  localhost ${user_input} www.${user_input};" >> /etc/nginx/sites/available/ghydra
-sudo echo " " >> /etc/nginx/sites/available/ghydra
-sudo echo "  location / {" >> /etc/nginx/sites/available/ghydra
-sudo echo "    include proxy_params;" >> /etc/nginx/sites/available/ghydra
-sudo echo "    proxy_pass http://unix:${current_dir}/ghydra.sock" >> /etc/nginx/sites/available/ghydra
-sudo echo "  }" >> /etc/nginx/sites/available/ghydra
-sudo echo "}" >> /etc/nginx/sites/available/ghydra
+sudo echo "server {" > /etc/nginx/sites-available/ghydra
+sudo echo "  listen 80;" >> /etc/nginx/sites-available/ghydra
+sudo echo "  localhost ${user_input} www.${user_input};" >> /etc/nginx/sites-available/ghydra
+sudo echo " " >> /etc/nginx/sites-available/ghydra
+sudo echo "  location / {" >> /etc/nginx/sites-available/ghydra
+sudo echo "    include proxy_params;" >> /etc/nginx/sites-available/ghydra
+sudo echo "    proxy_pass http://unix:${current_dir}/ghydra.sock" >> /etc/nginx/sites-available/ghydra
+sudo echo "  }" >> /etc/nginx/sites-available/ghydra
+sudo echo "}" >> /etc/nginx/sites-available/ghydra
 
 sudo ln -s /etc/nginx/sites-available/ghydra /etc/nginx/sites-enabled
 
