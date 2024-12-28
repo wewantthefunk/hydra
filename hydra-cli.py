a88b6cf36a5ffdd35315c6ad5a0823b1e7aac713
wewantthefunk <wewantthefunk@yahoo.com>
2024-12-28 17:46:34 -0500

    bug fixes

--- hydra-cli.py
+++ hydra-cli.py
@@ -13,6 +13,8 @@ if __name__ == '__main__':
 
         print('Marking user "' + sys.argv[2] + '" as Unverified')
         print(businesslogic.unverify_user(sys.argv[2]))
+        exit(0)
 
     if sys.argv[1] == 'login':
         print(businesslogic.login(sys.argv[2], sys.argv[3], sys.argv[4]))
+        exit(0)
